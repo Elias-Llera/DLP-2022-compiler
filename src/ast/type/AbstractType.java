16075baf7fa4c2ae8a8906403251ca6f615a7a21
Elias <eliaslleragr@gmail.com>
2022-04-10 12:10:40 +0200

    Abstract type methods implemented. Next: concrete implementations

--- src/ast/type/AbstractType.java
+++ src/ast/type/AbstractType.java
@@ -1,9 +1,61 @@
 package ast.type;
 
 import ast.AbstractAstNode;
+import ast.AstNode;
+
+import java.util.List;
 
 public abstract class AbstractType extends AbstractAstNode implements Type {
+
     public AbstractType(int line, int column) {
         super(line, column);
+    }
+
+    public boolean isLogical(){
+        return false;
+    }
+
+    public Type arithmetic(Type type, AstNode node){
+        return new ErrorType("Cannot do arithmetics operations with these types.", node.getLine(), node.getColumn());
+    }
+
+    public Type arithmetic(AstNode node){
+        return new ErrorType("Cannot do arithmetic operations with this type.", node.getLine(), node.getColumn());
+    }
+
+    public Type comparison(Type type, AstNode node){
+        return new ErrorType("Cannot do a comparison between these types.", node.getLine(), node.getColumn());
+    }
+
+    public Type logical(Type type, AstNode node){
+        return new ErrorType("Cannot do a logical operation with these types.", node.getLine(), node.getColumn());
+    }
+
+    public Type logical(AstNode node){
+        return new ErrorType("Cannot do a logical operation with this type.", node.getLine(), node.getColumn());
+    }
+
+    public Type dot(String name, AstNode node){
+        return new ErrorType("The type is not struct.", node.getLine(), node.getColumn());
+    }
+
+    public Type squareBrackets(Type type, AstNode node){
+        return new ErrorType("The type is not array.", node.getLine(), node.getColumn());
+    }
+
+    public boolean isBuiltIn(){
+        return false;
+    }
+
+    public Type canBeCast(Type type, AstNode node){
+        return new ErrorType("Cannot do cast operation.", node.getLine(), node.getColumn());
+    }
+
+    public Type parenthesis(List<Type> params, AstNode node){
+        return new ErrorType("The type is not function.", node.getLine(), node.getColumn());
+    }
+
+    public Type promotesTo(Type type, AstNode node){
+        return new ErrorType("Promotion is not allowed for this type.", node.getLine(), node.getColumn());
     }
 }
--- src/ast/type/Type.java
+++ src/ast/type/Type.java
@@ -2,5 +2,32 @@ package ast.type;
 
 import ast.AstNode;
 
+import java.util.List;
+
 public interface Type extends AstNode {
+
+    boolean isLogical();
+
+    Type arithmetic(Type type, AstNode node);
+
+    Type arithmetic(AstNode node);
+
+    Type comparison(Type type, AstNode node);
+
+    Type logical(Type type, AstNode node);
+
+    Type logical(AstNode node);
+
+    Type dot(String name, AstNode node);
+
+    Type squareBrackets(Type type, AstNode node);
+
+    boolean isBuiltIn();
+
+    Type canBeCast(Type type, AstNode node);
+
+    Type parenthesis(List<Type> params, AstNode node);
+
+    Type promotesTo(Type type, AstNode node);
+
 }
